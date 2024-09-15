de97c96f8b38be08722a5e8574a795a66147f098
Vidushi Sharma <23ucc614@lnmiit.ac.in>
2024-09-15 18:02:43 +0530

    Time: 330 ms (78.78%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-select distinct p1.email as Email from Person p1 join Person p2
-on p1.email = p2.email and p1.id <> p2.id;
+select distinct p1.email as Email from Person p1, Person p2
+where p1.email = p2.email and p1.id <> p2.id;
