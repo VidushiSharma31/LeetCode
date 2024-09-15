97ac06ca21c409ca5ff342c14b153442e4636143
Vidushi Sharma <23ucc614@lnmiit.ac.in>
2024-09-15 13:50:22 +0530

    Time: 0 ms (100%), Space: 9.5 MB (87.15%) - LeetHub

--- 0027-remove-element/0027-remove-element.c
+++ 0027-remove-element/0027-remove-element.c
@@ -2,7 +2,6 @@ int removeElement(int* nums, int numsSize, int val) {
     int k = 0;
     for(int i = 0; i < numsSize; i++) {
         if (nums[i] == val) {
-            nums[i] = NULL;
             k++;
         }
         else {
